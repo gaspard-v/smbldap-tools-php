332358f2ad171b62fcbb1d791101e241f4ae8fee
gaspard-v <60385905+gaspard-v@users.noreply.github.com>
2024-02-27 15:03:33 +0000

    Add ldap users

---
+++ debian-setup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+### PLEASE use 123 for ldap admin password!
+set -euo pipefail
+
+sudo apt update
+sudo apt install samba smbldap-tools slapd ldap-utils -y
+
+sudo cp --verbose --force "/etc/samba/smb.conf" "/etc/samba/smb.conf.origin"
+sudo cp --verbose --force "smb.conf" "/etc/samba/smb.conf"
+sudo chown root:root "/etc/samba/smb.conf"
+
+## test.org
+## dc=test,dc=org
+sudo dpkg-reconfigure slapd
+
+sudo service slapd restart
+cat "/usr/share/doc/samba/examples/LDAP/samba.ldif" | sudo ldapadd -Q -Y EXTERNAL -H ldapi:///
+
+sudo ldapmodify -Q -Y EXTERNAL -H ldapi:/// -f samba_indices.ldif
+sudo service slapd restart
+
+sudo service smbd restart
+
+#sudo smbldap-config
+sudp cp --verbose --force "/etc/smbldap-tools/smbldap_bind.conf" "/etc/smbldap-tools/smbldap_bind.conf.origin"
+sudo cp --verbose --force "smbldap_bind.conf" "/etc/smbldap-tools/smbldap_bind.conf"
+sudo chown root:root "/etc/smbldap-tools/smbldap_bind.conf"
+sudo chmod 600 "/etc/smbldap-tools/smbldap_bind.conf"
+
+sudo slapcat -l backup.ldif
+sudo smbldap-populate -g 10000 -u 10000 -r 10000
+
+sudo smbpasswd -w 123
+sudo service smbd restart
+sudo service nmbd restart
+
+sudo apt install libnss-ldap -y
+
+sudo service smbd restart
+sudo service nmbd restart
+
+sudo useradd --create-home --home=/media/hdd1/usertest --user-group --comment=usertest usertest
+sudo smbldap-useradd -a -P -m usertest
