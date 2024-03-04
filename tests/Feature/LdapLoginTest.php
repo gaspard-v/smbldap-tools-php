27e710db053ed72eac8579e43b474d9c4cf5acad
gaspard-v <60385905+gaspard-v@users.noreply.github.com>
2024-03-04 09:37:14 +0000

    Add Ldap and JWT files

---
+++ tests/Feature/LdapLoginTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use LdapRecord\Container;
+use LdapRecord\Models\OpenLDAP\User;
+use Tests\TestCase;
+
+class LdapLoginTest extends TestCase
+{
+    /**
+     * Test LDAP user connection
+     * username: usertest
+     * password: 123
+     */
+    public function test_login(): void
+    {
+        $connection = Container::getDefaultConnection();
+        $user = User::findByOrFail('sn', 'usertest');
+        $dn = $user->getDn();
+        $testResonse = $connection->auth()->attempt($dn, '123');
+        $this->assertTrue($testResonse);
+    }
+}
